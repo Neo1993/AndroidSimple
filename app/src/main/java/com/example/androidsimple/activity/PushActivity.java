da41c5d6a5c1a232acb3e137899bf3c6c7a583fb
Neo <melijun@163.com>
2020-12-27 21:19:05 +0800

    完善推送

--- app/src/main/java/com/example/androidsimple/activity/PushActivity.java
+++ app/src/main/java/com/example/androidsimple/activity/PushActivity.java
@@ -4,13 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.animation.LinearInterpolator;
 import android.widget.TextView;
 
 import com.example.androidsimple.R;
 import com.example.androidsimple.base.BaseActivity;
+import com.umeng.message.UmengNotifyClickActivity;
 
-public class PushActivity extends BaseActivity {
+import org.android.agoo.common.AgooConstants;
+
+public class PushActivity extends UmengNotifyClickActivity {
+    private static final String TAG = PushActivity.class.getSimpleName();
     private TextView pushShowTV;
 
     public static void startActivity(Activity activity, Bundle bundle){
@@ -23,6 +29,25 @@ public class PushActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_push);
         initView();
+    }
+
+    @Override
+    public void onMessage(Intent intent) {
+        super.onMessage(intent);
+        final String body = intent.getStringExtra(AgooConstants.MESSAGE_BODY);
+        final String extra = intent.getStringExtra(AgooConstants.MESSAGE_ACCS_EXTRA);
+        final String ext = intent.getStringExtra(AgooConstants.MESSAGE_EXT);
+        Log.i(TAG, body);
+        Log.i(TAG, "extara: " + extra);
+        Log.i(TAG, "ext: " + ext);
+        if (!TextUtils.isEmpty(body)) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    pushShowTV.setText(body);
+                }
+            });
+        }
     }
 
     private void initView(){
